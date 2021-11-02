103074b5eea5ad1724bc625a8a9b9ce6d0b73991
Chase <Chasze@gmail.com>
2021-11-02 13:03:18 -0500

    added colors to snake

--- gametext.py
+++ gametext.py
@@ -7,11 +7,15 @@ class GameText(Turtle):
         self.penup()
         self.score = 0
         self.hideturtle()
-        self.goto(-20, 280)
+        self.goto(-20, 260)
         self.color('White')
 
     def write_score(self):
-        self.write(f'Score: {self.score}')
+        self.write(f'Score: {self.score}', align='center', font=('Courier', 16, 'normal'))
+
+    def game_over(self):
+        self.goto(0 ,0)
+        self.write('GAME OVER' , align='center', font=('Courier', 16, 'normal'))
 
     def update_score(self):
         self.score += 1
--- main.py
+++ main.py
@@ -34,7 +34,24 @@ while game_is_on:
     # Detect collision with food using turtle.distance()
     if snake.head.distance(food) < 15:
         food.refresh()
+        snake.extent()
         text.update_score()
+    # Detect Collision with wall if snake is too close to walls
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on  = False
+        text.game_over()
+
+    # Detect Collision with tail
+    # if head collides with any tail segment, game over
+    # can improve this with slicing
+    for segment in snake.segments[1:]:
+        # get the segments starting at 2
+        # if segment == snake.head:
+  #  code w/o slice       pass
+        # elif
+        if snake.head.distance(segment) < 10:
+            text.game_over()
+            game_is_on = False
 
 
 screen.exitonclick()
